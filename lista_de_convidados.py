016a83d1ea3b6f01e7e53d0e521a700aacd8e620
AllanCorte <allanalvescorte@gmail.com>
2025-09-01 21:39:56 -0400

    att

--- lista_de_convidados.py
+++
@@ -1,7 +0,0 @@
-# 1.	Lista de Convidados
-# Crie uma lista com nomes de convidados para uma festa.
-#  Exiba uma mensagem personalizada para cada convidado
-convidados = ["allan", "lucas", "thiago", "ana"]
-
-for nome in convidados:
-    print(f'boa noite, sr {nome}')
--- tabuada.py
+++
@@ -1,7 +0,0 @@
-# Tabuada de um Número
-# Peça ao usuário um número inteiro e exiba a tabuada desse número de 1 a 10.
-
-numero = int(input('digite  um numero interio para a taboada: '))
-
-for i in range(0, 11):
-    print(f'{numero} x {i} = {numero * i}')
